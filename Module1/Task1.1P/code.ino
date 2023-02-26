9902e92f5a8654544c34aff9c1c238c8c28fc887
DeadStalker69 <106471305+DeadStalker69@users.noreply.github.com>
2023-02-26 14:51:53 +0530

    Update code.ino

--- Module1/Task1.1P/code.ino
+++ Module1/Task1.1P/code.ino
@@ -19,3 +19,9 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+//the motion sensor and arduino are connected via a digital pin. 
+//therefore, arduino and PIR sensor cannot communicate with values. digtal signal only works with 0 nad 1 as values and nothing more. 
+//therefore, motion sensor constantly sends a 0volts to the arduino unless motion is detected in its range of vision. 
+//if motion is deteced, it alters the voltage sent to 5v indicating a 1 in binary language. according to the delay input in the code, arduino rechecks for a signal from PIR sensor.
+//if voltage is dropped down to 0, then it understands that motion have been stopped.  
