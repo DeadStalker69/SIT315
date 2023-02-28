3e2684371f310a19cf9278fe85412f68215d5f55
DeadStalker69 <106471305+DeadStalker69@users.noreply.github.com>
2023-02-28 10:00:56 +0530

    Update code.ino

--- Module1/Task1.2P/code.ino
+++ Module1/Task1.2P/code.ino
@@ -1,24 +1,26 @@
-uint8_t LedState = LOW;
+uint8_t LedState = LOW;     //defining a variable which we will use in the interrupt to turn the LED off.
 
 void setup() {
-  pinMode(2, INPUT_PULLUP);      
+  pinMode(2, INPUT_PULLUP);      //defining input pin of PIR sensor and also use an internal pullup resistor of the pin.
   pinMode(3, OUTPUT);
 
-  attachInterrupt(digitalPinToInterrupt(2), LedOn, RISING);
+  attachInterrupt(digitalPinToInterrupt(2), LedOn, RISING);   //starting the interrupt on GPIO2, with LedOn as the function adn RISING as the change on which interrupt triggers.
   Serial.begin(9600);
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
 
-  delay;
-  // put your main code here, to run repeatedly:
+  delay(500);
 
 }
 
-void LedOn()
+void LedOn()      //the function which will be ran after the interrupt is triggered.
 {
-  LedState = !LedState;
-  digitalWrite(3, LedState);
+  LedState = !LedState;   //change the LED state.
+  digitalWrite(3, LedState);    //switch the LED on or off based on the value in the variable.
 }
+
+//this code works asme as the previous code but we are using interrrupts in place of simple program.
+//interrupts are a short and quick commands in a code which (as the name suggests) interrupts the normal flow of the code.
+//if an interrupt is triggered, it stops the code whereever it is, runs the code of the interrupt and then resumes the normal flow of the code.
