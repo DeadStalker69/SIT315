174bec110566f7e837a9e91c4b87fbadd3cff308
DeadStalker69 <106471305+DeadStalker69@users.noreply.github.com>
2023-03-14 02:21:10 +0530

    Update code.ino

--- Module1/task1.3C/code.ino
+++ Module1/task1.3C/code.ino
@@ -1,13 +1,13 @@
-uint8_t LedState = LOW;
+uint8_t LedState = LOW;   //constant variable to define default LED state.
 
 void setup() {
-  pinMode(2, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);   //using buiilt-in pullup resistor for PIR sensor
   pinMode(3, INPUT);      
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
 
-  attachInterrupt(digitalPinToInterrupt(2), RedLedOn, RISING);
-  attachInterrupt(digitalPinToInterrupt(3), GreenLedOn, FALLING);
+  attachInterrupt(digitalPinToInterrupt(2), RedLedOn, RISING);    //adding a rising interrupt by PIR sensor which stops the code and turns on Red LED once.
+  attachInterrupt(digitalPinToInterrupt(3), GreenLedOn, FALLING);   //adding a falling interrupt by push button which stops the code and turns on Green LED once.
   Serial.begin(9600);
   // put your setup code here, to run once:
 
@@ -15,19 +15,19 @@ void setup() {
 
 void loop() {
 
-  delay(500);
+  delay(500);         // a constant delay is added to potray arduino as being busy in some task, which will b esuspended by interrupt later
   // put your main code here, to run repeatedly:
 
 }
 
-void RedLedOn()
+void RedLedOn()     //a function called if PIR sensor detects motion.
 {
   Serial.println("Change have been detected by Motion sensor");
-  LedState = !LedState;
-  digitalWrite(4, LedState);
+  LedState = !LedState;   //turn the LED stated to opposite than current state. by default, change it to on from off.
+  digitalWrite(4, LedState);    //reflect the changes state on the LED
 }
 
-void GreenLedOn()
+void GreenLedOn()     //a function called if button is pressed
 {
   Serial.println("Button have been pressed.");
   LedState = !LedState;
